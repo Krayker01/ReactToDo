253ee98024c8b5a3b8fe6352d1532cbf5c8dbe2e
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-11-09 18:01:15 -0500

    feat: complete server setup and add rate limiting - Finished backend implementation - Integrated Upstash rate limiter for API requests - Minor cleanup in server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 const app = express();
 import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "../config/db.js";
+import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-connectDB();
-
 // middleware
 app.use(express.json());
-
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server was started on PORT:", PORT));
+connectDB().then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log("Server was started on PORT:", PORT));
+});
