1ec3b790bec3a5ddf0b8e298324564fe57e8c61b
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-11-20 21:02:30 -0500

    feat: add isUpdated state to NoteCard to track if note was edited

--- frontend/thinkboard/src/components/NoteCard.jsx
+++ frontend/thinkboard/src/components/NoteCard.jsx
@@ -1,11 +1,18 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils'
 import api from '../lib/axios'
 import toast from 'react-hot-toast'
 
 const NoteCard = ({ note, setNotes }) => {
+    const [isUpdated, setIsUpdated] = useState(false);
+    useEffect(() => {
+        const created = new Date(note.createdAt).toISOString();
+        const updated = new Date(note.updatedAt).toISOString();
+
+        setIsUpdated(created !== updated);
+    }, [note.createdAt, note.updatedAt]);
     const handleDelete = async (e, id) => {
         e.preventDefault();
         try {
@@ -32,15 +39,28 @@ const NoteCard = ({ note, setNotes }) => {
             <h3 className='card-title text-base-content'>{note.title}</h3>
             <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
             <div className='card-actions justify-between items-center mt-4'>
-                <span className='text-sm text-base-content/60'>
-                    {formatDate(new Date(note.createdAt))}
-                </span>
+                <div className="flex flex-col text-xs text-base-content/60 leading-tight">
+                    <span>
+                        <span className="font-semibold">Created:</span>{" "}
+                        {formatDate(new Date(note.createdAt))}
+                    </span>
+                    {isUpdated && (
+                        <span>
+                            <span className="font-semibold">Updated:</span>{" "}
+                            {formatDate(new Date(note.updatedAt))}
+                        </span>
+                    )}
+                </div>
                 <div className='flex items-center gap-1'>
                     <PenSquareIcon className='size-4' />
                     <button className='btn btn-ghost btn-xs text-error'>
-                        <Trash2Icon className='size-4' onClick={(e) => { handleDelete(e, note._id) }} />
+                        <Trash2Icon
+                            className='size-4'
+                            onClick={(e) => handleDelete(e, note._id)}
+                        />
                     </button>
                 </div>
+
             </div>
         </div>
     </Link>
