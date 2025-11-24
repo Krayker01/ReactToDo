03f60617675a4ba2a5e4c124ac295386c2b2b0af
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-11-23 19:54:06 -0500

    Prerpared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,36 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+const __dirname = path.resolve();
 
 dotenv.config();
 const app = express();
 
 // middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/thinkboard/dist")));
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/thinkboard/dist/index.html"));
+    });
+};
 
 connectDB().then(() => {
     const PORT = process.env.PORT || 3000;
